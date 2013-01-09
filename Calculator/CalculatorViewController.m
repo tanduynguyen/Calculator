88387c4cedc8c53a2df83f6518b1f523009fac04
tanduynguyen <tanduy.nguyen@2359media.us>
2013-01-09 08:44:44 +0700

    tanduy implemented task 1, 2, 3

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -11,6 +11,7 @@
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL alreadyDot;
 @property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
@@ -22,17 +23,38 @@
     }
     return _brain;
 }
+
 - (IBAction)digitPressed:(UIButton *)sender {
-    NSString *digit = sender.currentTitle;
+    NSString *digit = sender.currentTitle;    
+    if ([self.display.text isEqualToString:@"0"]) {
+        
+        if ([digit isEqualToString:@"0"]) {
+            self.alreadyDot = NO;
+            self.userIsInTheMiddleOfEnteringANumber = YES;
+            return;
+        } else if ([digit isEqualToString:@"."]){
+            self.userIsInTheMiddleOfEnteringANumber = YES;
+        } else {
+            self.userIsInTheMiddleOfEnteringANumber = NO;
+        }
+    }     
+    
     if (self.userIsInTheMiddleOfEnteringANumber)
     {
-    
-        self.display.text = [self.display.text stringByAppendingString:digit];
+        if (!self.alreadyDot || ![digit isEqualToString:@"."]) {
+            self.display.text = [self.display.text stringByAppendingString:digit];
+            if ([digit isEqualToString:@"."]) {
+                self.alreadyDot = YES;
+            }
+            
+        }
     }
     else
     {
-        self.display.text = digit;
-        self.userIsInTheMiddleOfEnteringANumber = YES;
+        if (![digit isEqualToString:@"."]) {
+            self.display.text = digit;
+            self.userIsInTheMiddleOfEnteringANumber = YES;
+        }
     }
 }
 
@@ -48,9 +70,17 @@
     self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
+
+- (IBAction)signPressed {
+    if ([self.display.text doubleValue] > 0) {
+        self.display.text = [@"-" stringByAppendingString:self.display.text];
+    }
+}
+
 - (IBAction)enterPressed {
     [self.brain pushOperand:[self.display.text doubleValue]];
     self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.alreadyDot = NO;
 }
 
 @end
