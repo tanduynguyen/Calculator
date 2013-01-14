29530b1ed84d71619e605ef562f0fae37e8d7725
Tan Duy <tanduy.nguyen@2359media.us>
2013-01-14 18:14:58 +0700

    DUY QUOC

---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by viet on 1/14/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
---
+++ Calculator/GraphViewController.m
@@ -0,0 +1,38 @@
+//
+//  GraphViewController.m
+//  Calculator
+//
+//  Created by viet on 1/14/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
