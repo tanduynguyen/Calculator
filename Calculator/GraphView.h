38c390200665c559eefd039756bf5fa88aa51131
Tan Duy <tanduy.nguyen@2359media.us>
2013-01-16 17:34:08 +0700

    Already draw axes but not finish

---
+++ Calculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by viet on 1/15/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ Calculator/GraphView.m
@@ -0,0 +1,31 @@
+//
+//  GraphView.m
+//  Calculator
+//
+//  Created by viet on 1/15/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
